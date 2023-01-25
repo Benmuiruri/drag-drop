8399ed45f64e68cfe2ae17e4fba8e197809ea7bb
Benmuiruri <ben.muiruri13@gmail.com>
2023-01-25 13:59:17 +0300

    render form to the DOM

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,25 @@
-// Code goes here!
+class ProjectInput {
+  formTemplateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+
+  constructor() {
+    // select the template element
+    this.formTemplateElement = document.getElementById('project-input') as HTMLTemplateElement;
+    this.hostElement = document.getElementById('app') as HTMLDivElement;
+
+    // get form inside the template
+    const importedFormNode = document.importNode(this.formTemplateElement.content, true);
+    this.element = importedFormNode.firstElementChild as HTMLFormElement;
+
+    // attach the form to the DOM
+    this.attach();
+
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement('afterbegin', this.element);
+  }
+}
+
+const projectInput = new ProjectInput();
