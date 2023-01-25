6c92d4d0ea8d2a37fcf3f01d5b264ab92b070ecf
Benmuiruri <ben.muiruri13@gmail.com>
2023-01-25 14:11:14 +0300

    Add autobind Decorator for accessing form input

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,26 @@
+// Autobind decorator
+function Autobind(_1: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    enumerable: false,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    }
+  };
+  return adjDescriptor;
+}
+
+// ProjectInput class
 class ProjectInput {
   formTemplateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
+
 
   constructor() {
     // select the template element
@@ -11,14 +30,31 @@ class ProjectInput {
     // get form inside the template
     const importedFormNode = document.importNode(this.formTemplateElement.content, true);
     this.element = importedFormNode.firstElementChild as HTMLFormElement;
+    this.element.id = 'user-input';
+
+    // Access the form inputs
+    this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;
 
     // attach the form to the DOM
-    this.attach();
+    this.configureForm();
+    this.attachForm();
 
   }
 
-  private attach() {
+  private attachForm() {
     this.hostElement.insertAdjacentElement('afterbegin', this.element);
+  }
+
+  @Autobind
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+  }
+
+  private configureForm() {
+    this.element.addEventListener('submit', this.submitHandler);
   }
 }
 
