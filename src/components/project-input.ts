416162f189ea0eb398c1c0d197fd0f887615cd83
Benmuiruri <ben.muiruri13@gmail.com>
2023-02-07 12:54:57 +0300

    Feat: bundle code with Webpack

--- src/components/project-input.ts
+++ src/components/project-input.ts
@@ -1,7 +1,7 @@
-import Component from './base-component.js';
-import * as Validation from '../utils/validation.js';
-import { Autobind } from '../decorators/autobind.js';
-import { projectState } from '../state/project-state.js';
+import Component from './base-component';
+import * as Validation from '../utils/validation';
+import { Autobind } from '../decorators/autobind';
+import { projectState } from '../state/project-state';
 
 // ProjectInput class
 export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement>  {
---
+++ webpack.config.prod.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/app.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  devtool: false,
+  devServer: {
+    static: {
+      directory: path.join(__dirname, '/'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  }
+};
