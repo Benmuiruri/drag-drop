09e7987e102f2283280d4c3985ca6ec6f076c29c
Benmuiruri <ben.muiruri13@gmail.com>
2023-02-07 08:53:01 +0300

    Chore: add getter to display no of people

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-// Project type 
+// Project type
 
 enum ProjectStatus {
   Active = "active",
@@ -219,6 +219,14 @@ class ProjectInput extends Component<HTMLDivElement, HTMLFormElement>  {
 class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
   private project: Project;
 
+  get persons(){
+    if(this.project.people === 1){
+      return '1 person';
+    } else {
+      return `${this.project.people} people`;
+    }
+  }
+
   constructor(hostId: string, project: Project) {
     super('single-project', hostId, false, project.id);
     this.project = project;
@@ -229,9 +237,9 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
 
   configure() {}
 
-  renderContent() { 
+  renderContent() {
     this.element.querySelector('h2')!.textContent = this.project.title;
-    this.element.querySelector('h3')!.textContent = this.project.people.toString() + ' assigned';
+    this.element.querySelector('h3')!.textContent = this.persons + ' assigned';
     this.element.querySelector('p')!.textContent = this.project.description;
   }
 }
